0bbe53653c54a3d7002d68e568b0a042ea7808d6
Marcon Willian <marconwillian@protonmail.com>
2020-05-02 22:27:03 -0400

    Create types of mutation

--- src/types/index.js
+++ src/types/index.js
@@ -5,9 +5,11 @@ const fs = require('fs'),
 const UserTypes = fs.readFileSync(`${dirName}/User/types.graphql`, 'utf8');
 const PageInfoTypes = fs.readFileSync(`${dirName}/PageInfo/types.graphql`, 'utf8');
 const QueryTypes = fs.readFileSync(`${dirName}/Query/types.graphql`, 'utf8');
+const MutationTypes = fs.readFileSync(`${dirName}/Mutation/types.graphql`, 'utf8');
 
 module.exports = () => `
     ${UserTypes}
     ${PageInfoTypes}
     ${QueryTypes}
+    ${MutationTypes}
 `;
