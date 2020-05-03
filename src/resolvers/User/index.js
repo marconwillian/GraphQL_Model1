3e36f619c98f53eb01a68b1a4973f5594091b9c5
Marcon Willian <marconwillian@protonmail.com>
2020-05-02 21:12:33 -0400

    Create Resolvers

---
+++ src/resolvers/User/index.js
@@ -0,0 +1,39 @@
+const usersList = [
+    {id: 1, name: "Marcon", email: "marcon@mh4sh.dev", age: "21"},
+    {id: 2, name: "Murillo", email: "murillo@mh4sh.dev"},
+    {id: 3, name: "dÉ", email: "DE@mh4sh.dev", age: "47"}
+];
+
+const pageInfo = {
+  endCursor: "CURSOR NÃO ARRUMADO",
+  hasNextPage: true
+}
+
+
+const users = async (_, args) => {
+  try {
+    const current = "CURSOR NÃO ARRUMADO"
+    console.log(args);
+    return {
+      pageInfo,
+      edges: usersList.map(item => ({ node: item, cursor: current })),
+    };
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
+
+const user = async (_, args) => {
+  try {
+    console.log(args);
+    const { data } = usersList.find(user => user.id === args.id);
+    return data;
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
+
+module.exports = {
+    users,
+    user,
+};
